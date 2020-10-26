a40ca2f8f97c854ba7549e0c83caeabbb7fb76e1
charliehb1 <61828414+charliehb1@users.noreply.github.com>
2020-10-26 10:09:28 +0000

    Update T4.py

--- T4.py
+++ T4.py
@@ -5,8 +5,11 @@ Created on Thu Oct 15 13:50:42 2020
 @author: charl
 """
 
+def calc():
+  
+    num1 = input("First Number: ");
+    num2 = input("Second Number: ");
+    print("Sum: "+str(int(num1)+int(num2)))
+    print("Difference: "+str(int(num1)-int(num2)))
 
-num1 = input("First Number: ");
-num2 = input("Second Number: ");
-print("Sum: "+str(int(num1)+int(num2)))
-print("Difference: "+str(int(num1)-int(num2)))
+calc()
